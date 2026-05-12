a69c680d71ffa5f7d698fa0b74986402feab3ffc
begalve-lopes <begalve.927390438.00@gmail.com>
2026-05-12 12:05:41 +0100

    criar o testes e pages object

--- e2e/login.spec.ts
+++ e2e/login.spec.ts
@@ -1,30 +1,44 @@
 import { test, expect } from '@playwright/test';
+import { obterCodigo2FA } from '../support/db';
+import { Usuario } from '../support/interface/usuario.interface';
+import { LoginPage } from '../support/pages/loginPage';
+import { DashPage } from '../support/pages/dashPage';
 
-interface Usuario{
-  cpf: string;
-  senha: string;
-}
 
 test('Não deve logar quando o código for inválido', async ({ page }) => {
-  const usuario:Usuario = {
+  const usuario: Usuario = {
     cpf: '00000014141',
     senha: '147258'
   }
 
-  await page.goto('http://paybank-mf-auth:3000/');
+  const lopinPage = new LoginPage(page);
+  await lopinPage.go()
+  await lopinPage.informarCpf(usuario.cpf)
+  await lopinPage.informarSenha(usuario.senha)
+  await lopinPage.informarCodigo2FA('123456')
+  await lopinPage.CodigoInvalido('Código inválido. Por favor, tente novamente.')
 
-  await page.getByRole('textbox', { name: 'Digite seu CPF' }).fill(usuario.cpf);
-  await page.getByRole('button', { name: 'Continuar' }).click();
+});
 
-  for(const digito of usuario.senha){
 
-    await page.getByRole('button', { name: digito }).click();
+test('Deve acessar a conta quando o código for válido', async ({ page }) => {
+
+  const usuario: Usuario = {
+    cpf: '00000014141',
+    senha: '147258'
   }
-  await page.getByRole('button', { name: 'Continuar' }).click();
 
-  await page.getByRole('textbox', { name: '000000' }).fill('123456');
-  await page.getByRole('button', { name: 'Verificar' }).click();
+  const lopinPage = new LoginPage(page);
+  await lopinPage.go()
+  await lopinPage.informarCpf(usuario.cpf)
+  await lopinPage.informarSenha(usuario.senha)
 
-  await expect(page.locator('span')).toContainText('Código inválido. Por favor, tente novamente.');
+  await page.waitForTimeout(3000) // Espera para garantir que a página tenha carregado completamente
+  const codigo2FA = await obterCodigo2FA();
+  await lopinPage.informarCodigo2FA(codigo2FA)
 
+  await page.waitForTimeout(2000) // Espera para garantir que a página tenha carregado completamente
+  
+  const dashPage = new DashPage(page);
+  await dashPage.obterSaldo('R$ 5.000,00')
 });
